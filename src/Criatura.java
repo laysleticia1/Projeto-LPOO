a9a47a1872deac38a84c5537c20073c9fe3325e0
mabjr33 <marcelinhoabjr@gmail.com>
2025-03-17 17:42:28 -0300

    Adicionando classes Criatura e Ambiente

--- src/Criatura.java
+++ src/Criatura.java
@@ -1,2 +1,10 @@
 public class Criatura {
+    private String nome;
+    private String tipo;
+    private int nivelPerigo;
+    private int vida;
+    private int dano;
+    private int velocidade;
+    private String habitat;
+    private boolean hostil;
 }
