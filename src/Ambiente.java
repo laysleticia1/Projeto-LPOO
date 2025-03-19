95a83b27ba032e9ddbecc836cf937e13bfe0555a
mabjr33 <marcelinhoabjr@gmail.com>
2025-03-19 19:06:57 -0300

    Métodos ambiente

--- src/Ambiente.java
+++ src/Ambiente.java
@@ -5,4 +5,50 @@ public abstract class Ambiente {
     private String[] recursosDisponiveis;
     private double probabilidadeEventos;
     private String condicaoClimatica;
+
+    void explorar(){
+
+    }
+    void gerarEvento(){
+
+    }
+    void modificarClima(){
+
+    }
+    String getNome(){
+        return nome;
+    }
+    void setNome(String nomeAmbiente){
+        this.nome = nomeAmbiente;
+    }
+    String getDescricao(){
+        return descricao;
+    }
+    void setDescricao(String descricaoAmbiente){
+        this.descricao = descricaoAmbiente;
+    }
+    int getDificuldadeExploracao(){
+        return dificuldadeExploracao;
+    }
+    void setDificuldadeExploracao(int dificuldade){
+        this.dificuldadeExploracao = dificuldade;
+    }
+    String[] getRecursosDisponiveis(){
+        return recursosDisponiveis;
+    }
+    void setRecursosDisponiveis(String[] recursosDisponiveis){
+        this.recursosDisponiveis = recursosDisponiveis;
+    }
+    double getProbabilidadeEventos(){
+        return probabilidadeEventos;
+    }
+    void setProbabilidadeEventos(double probabilidade){
+        this.probabilidadeEventos = probabilidade;
+    }
+    String getCondicaoClimatica(){
+        return condicaoClimatica;
+    }
+    void setCondicaoClimatica(String condicao){
+        this.condicaoClimatica = condicao;
+    }
 }
